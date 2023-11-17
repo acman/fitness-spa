69e1a7935603fabb66cab01289845221cf78bcd9
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-17 13:43:47 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "grey-20": "#F8F4EB",
-        "grey-50": "#EFE6E6",
-        "grey-100": "#DFCCCC",
-        "grey-500": "#5E0000",
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
         "primary-100": "#FFE1E0",
-        "primary-200": "#FFA6A3",
-        "promary-500": "#FF6B66",
-        "secondary-400": "#FFCD58",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD5B",
         "secondary-500": "#FFC132",
       },
       backgroundImage: (theme) => ({
-        "gradient-yellowred": "linear-gradient(90deg, #FF6B66 0%, #FFC132 100%)",
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
         "mobile-home": "url('./assets/HomePageGraphic.png')",
       }),
       fontFamily: {
@@ -24,17 +25,16 @@ export default {
       },
       content: {
         evolvetext: "url('./assets/EvolveText.png')",
-        abstracetwaves: "url('./assets/AbstractWaves.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
         sparkles: "url('./assets/Sparkles.png')",
-        cirlces: "url('./assets/Circles.png')",
-      }
+        circles: "url('./assets/Circles.png')",
+      },
     },
     screens: {
-      "xs": "480px",
-      "sm": "768px",
-      "md": "1060px",
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
     },
   },
   plugins: [],
-}
-
+};
